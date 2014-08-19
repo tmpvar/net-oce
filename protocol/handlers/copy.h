e687fb73da99d230f7f6ea4e6bad4ab839e2fb38
Elijah Insua <tmpvar@gmail.com>
2014-08-19 13:04:30 -0700

    add copy

---
+++ protocol/handlers/copy.h
@@ -0,0 +1,28 @@
+#include "handler.h"
+
+#include <BRepBuilderAPI_Copy.hxx>
+
+HANDLER(copy, "handle") {
+
+  if (req->argument_size() != 1) {
+    HANDLER_ERROR("1 argument required: handle")
+    return true;
+  }
+
+  uint32_t handle = req->argument(0).uint32_value();
+
+  TopoDS_Shape shape = editor->getShape(handle);
+  if (shape.IsNull()) {
+    HANDLER_ERROR("specified object does not exist")
+    return true;
+  }
+
+  BRepBuilderAPI_Copy builder;
+  builder.Perform(shape);
+
+  NetOCE_Value *val = res->add_value();
+  val->set_type(NetOCE_Value::SHAPE_HANDLE);
+  val->set_uint32_value(editor->addShape(builder.Shape()));
+
+  return true;
+}
