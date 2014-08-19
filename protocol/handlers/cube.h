e0819516bc577415dc5f498307703965eb7f501e
Elijah Insua <tmpvar@gmail.com>
2014-08-18 23:32:01 -0700

    cube is now centered on origin by default

--- protocol/handlers/cube.h
+++ protocol/handlers/cube.h
@@ -2,22 +2,19 @@
 
 #include <BRepPrimAPI_MakeBox.hxx>
 
-HANDLER(cube, "double, double, double, double, double, double") {
+HANDLER(cube, "double, double, double") {
 
-  if (req->argument_size() != 6) {
-    HANDLER_ERROR("6 arguments required: x y z w d h")
+  if (req->argument_size() != 3) {
+    HANDLER_ERROR("3 arguments required: width(x) depth(y) height(z)")
     return true;
   }
 
-  double x = req->argument(0).double_value();
-  double y = req->argument(1).double_value();
-  double z = req->argument(2).double_value();
-  double w = req->argument(3).double_value();
-  double h = req->argument(4).double_value();
-  double d = req->argument(5).double_value();
+  double w = req->argument(0).double_value()/2;
+  double h = req->argument(1).double_value()/2;
+  double d = req->argument(2).double_value()/2;
 
 
-  TopoDS_Solid cube = BRepPrimAPI_MakeBox(gp_Pnt(x, y, z), w, h, d);
+  TopoDS_Solid cube = BRepPrimAPI_MakeBox(gp_Pnt(-w, -h, -d), gp_Pnt(w, h, d));
 
   NetOCE_Value *val = res->add_value();
   val->set_type(NetOCE_Value::SHAPE_HANDLE);
