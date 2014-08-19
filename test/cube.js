4de672fdf20b10d2ba2442fc2e9d9707da11d6ed
Elijah Insua <tmpvar@gmail.com>
2014-08-18 17:01:35 -0700

    add cube test

---
+++ test/cube.js
@@ -0,0 +1,14 @@
+var request = require('net-oce-protocol').request;
+
+process.stdout.write(request.encode({
+  method: 1,
+  seq: 1,
+  argument : [
+    { type : 1, double_value: 1 },
+    { type : 1, double_value: 1 },
+    { type : 1, double_value: 1 },
+    { type : 1, double_value: 10 },
+    { type : 1, double_value: 10 },
+    { type : 1, double_value: 10 }
+  ]
+}))
