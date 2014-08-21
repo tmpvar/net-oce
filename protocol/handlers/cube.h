561e470752fa3a84aee83fff1c882913097b9a84
Elijah Insua <tmpvar@gmail.com>
2014-08-20 23:36:51 -0700

    move cube, add box

--- protocol/handlers/cube.h
+++ protocol/handlers/cube.h
@@ -2,19 +2,16 @@
 
 #include <BRepPrimAPI_MakeBox.hxx>
 
-HANDLER(cube, "double, double, double") {
+HANDLER(cube, "double") {
 
   if (req->argument_size() != 3) {
-    HANDLER_ERROR("3 arguments required: width(x) depth(y) height(z)")
+    HANDLER_ERROR("r argument required: radius")
     return true;
   }
 
-  double w = req->argument(0).double_value()/2;
-  double h = req->argument(1).double_value()/2;
-  double d = req->argument(2).double_value()/2;
+  double r = req->argument(0).double_value()/2;
 
-
-  TopoDS_Solid cube = BRepPrimAPI_MakeBox(gp_Pnt(-w, -h, -d), gp_Pnt(w, h, d));
+  TopoDS_Solid cube = BRepPrimAPI_MakeBox(gp_Pnt(-r, -r, -r), gp_Pnt(r, r, r));
 
   NetOCE_Value *val = res->add_value();
   val->set_type(NetOCE_Value::SHAPE_HANDLE);
--- test/cube.js
+++ test/cube.js
@@ -4,9 +4,6 @@ process.stdout.write(request.encode({
   method: 1,
   seq: 1,
   argument : [
-    { type : 1, double_value: 1 },
-    { type : 1, double_value: 1 },
-    { type : 1, double_value: 1 },
     { type : 1, double_value: 10 },
     { type : 1, double_value: 10 },
     { type : 1, double_value: 10 }
