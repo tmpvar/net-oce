ee8dfd26fb50e9bb6bdf243a1690af199d4a4fe0
Elijah Insua <tmpvar@gmail.com>
2014-09-04 14:38:19 -0700

    shape_display exports multiple meshes (refs #46)

--- protocol/handlers/shape_display.h
+++ protocol/handlers/shape_display.h
@@ -17,137 +17,149 @@ HANDLER(shape_display, "handle, handle..") {
     HANDLER_ERROR(">= 1 arguments required: handle, handle..")
     return true;
   }
+
   if (editor->shapes->size()) {
-    TopoDS_Compound compoundShape;
-    BRep_Builder builder;
-    builder.MakeCompound(compoundShape);
+    Standard_Real x1, y1, z1;
+    Standard_Real x2, y2, z2;
+    Standard_Real x3, y3, z3;
 
-    uint32_t handle;
+    double bounds[6];
+    float theDeflection;
+    Bnd_Box total;
+    TopoDS_Shape shape;
+    Handle_StlMesh_Mesh mesh = new StlMesh_Mesh;
+    uint32_t total_triangles;
+    uint32_t position_size;
+    uint32_t normal_size;
+    uint32_t domain_size;
+    Standard_Integer domainCount;
+
+    uint32_t position_where;
+    uint32_t normal_where;
+    uint32_t domain_where;
+
     for (int i=0; i<argc; i++) {
-      handle = req->argument(i).uint32_value();
+      shape = editor->getShape(req->argument(i).uint32_value());
 
-      TopoDS_Shape shape = editor->getShape(handle);
       if (shape.IsNull()) {
         HANDLER_ERROR("specified object does not exist")
         return true;
       }
 
-      builder.Add(compoundShape, shape);
-    }
+      BRepBndLib::Add(shape, total);
 
-    Handle_StlMesh_Mesh mesh = new StlMesh_Mesh;
+      total.Get(
+        bounds[0],
+        bounds[1],
+        bounds[2],
+        bounds[3],
+        bounds[4],
+        bounds[5]
+      );
 
-    double bounds[6];
-    float theDeflection;
-    Bnd_Box total;
+      theDeflection = MAX3(
+        bounds[3]-bounds[0],
+        bounds[4]-bounds[1],
+        bounds[5]-bounds[2]
+      ) * 0.01;
 
-    BRepBndLib::Add(compoundShape, total);
+      // TODO: why am I newing here?
+      StlTransfer::BuildIncrementalMesh(shape, .1, true, mesh);
 
-    total.Get(bounds[0], bounds[1], bounds[2], bounds[3], bounds[4], bounds[5]);
+      total_triangles = mesh->NbTriangles();
+      position_size = total_triangles * 9 * sizeof(float);
+      normal_size = total_triangles * 9 * sizeof(float);
+      domain_size = total_triangles * sizeof(uint32_t);
 
-    theDeflection = MAX3(bounds[3]-bounds[0] , bounds[4]-bounds[1] , bounds[5]-bounds[2]) * 0.01;
-    StlTransfer::BuildIncrementalMesh(compoundShape, .1, true, mesh);
+      domainCount = mesh->NbDomains();
 
+      position_where = 0;
+      normal_where = 0;
+      domain_where = 0;
 
-    Standard_Real x1, y1, z1;
-    Standard_Real x2, y2, z2;
-    Standard_Real x3, y3, z3;
+      // we lose some precision here...
+      float *position = (float *)malloc(position_size);
+      float *normal = (float *)malloc(normal_size);
+      uint32_t *domains = (uint32_t *)malloc(domain_size);
+      StlMesh_MeshExplorer explorer(mesh);
 
-    uint32_t total_triangles = mesh->NbTriangles();
-    uint32_t position_size = total_triangles * 9 * sizeof(float);
-    uint32_t normal_size = total_triangles * 9 * sizeof(float);
-    uint32_t domain_size = total_triangles * sizeof(uint32_t);
+      for (uint32_t nbd = 1; nbd <= domainCount; nbd++) {
 
-    Standard_Integer domainCount = mesh->NbDomains();
+        for (explorer.InitTriangle(nbd); explorer.MoreTriangle(); explorer.NextTriangle()) {
+          explorer.TriangleVertices (
+            x1, y1, z1,
+            x2, y2, z2,
+            x3, y3, z3
+          );
 
-    uint32_t position_where = 0, normal_where = 0, domain_where = 0;
+          gp_XYZ v12 ((x2-x1), (y2-y1), (z2-z1));
+          gp_XYZ v13 ((x3-x1), (y3-y1), (z3-z1));
+          gp_XYZ norm = v12 ^ v13;
+          Standard_Real mod = norm.Modulus ();
+          if (mod > gp::Resolution()) {
+            norm.Divide(mod);
+          } else {
+            norm.SetCoord (0., 0., 0.);
+          }
 
-    // we lose some precision here...
-    float *position = (float *)malloc(position_size);
-    float *normal = (float *)malloc(normal_size);
-    uint32_t *domains = (uint32_t *)malloc(domain_size);
-    StlMesh_MeshExplorer explorer(mesh);
+          domains[domain_where++] = nbd;
 
-    // create progress sentry for domains
+          position[position_where++] = x1;
+          position[position_where++] = y1;
+          position[position_where++] = z1;
 
-    for (uint32_t nbd = 1; nbd <= domainCount; nbd++) {
+          position[position_where++] = x2;
+          position[position_where++] = y2;
+          position[position_where++] = z2;
 
-      for (explorer.InitTriangle(nbd); explorer.MoreTriangle(); explorer.NextTriangle()) {
-        explorer.TriangleVertices (
-          x1, y1, z1,
-          x2, y2, z2,
-          x3, y3, z3
-        );
+          position[position_where++] = x3;
+          position[position_where++] = y3;
+          position[position_where++] = z3;
 
-        gp_XYZ v12 ((x2-x1), (y2-y1), (z2-z1));
-        gp_XYZ v13 ((x3-x1), (y3-y1), (z3-z1));
-        gp_XYZ norm = v12 ^ v13;
-        Standard_Real mod = norm.Modulus ();
-        if (mod > gp::Resolution()) {
-          norm.Divide(mod);
-        } else {
-          norm.SetCoord (0., 0., 0.);
+          normal[normal_where++] = norm.X();
+          normal[normal_where++] = norm.Y();
+          normal[normal_where++] = norm.Z();
+
+          normal[normal_where++] = norm.X();
+          normal[normal_where++] = norm.Y();
+          normal[normal_where++] = norm.Z();
+
+          normal[normal_where++] = norm.X();
+          normal[normal_where++] = norm.Y();
+          normal[normal_where++] = norm.Z();
         }
-
-        domains[domain_where++] = nbd;
-
-        position[position_where++] = x1;
-        position[position_where++] = y1;
-        position[position_where++] = z1;
-
-        position[position_where++] = x2;
-        position[position_where++] = y2;
-        position[position_where++] = z2;
-
-        position[position_where++] = x3;
-        position[position_where++] = y3;
-        position[position_where++] = z3;
-
-        normal[normal_where++] = norm.X();
-        normal[normal_where++] = norm.Y();
-        normal[normal_where++] = norm.Z();
-
-        normal[normal_where++] = norm.X();
-        normal[normal_where++] = norm.Y();
-        normal[normal_where++] = norm.Z();
-
-        normal[normal_where++] = norm.X();
-        normal[normal_where++] = norm.Y();
-        normal[normal_where++] = norm.Z();
       }
+
+      mesh->Clear();
+
+      NetOCE_Value *object = res->add_value();
+      object->set_type(NetOCE_Value::OBJECT);
+
+      NetOCE_Value *positionVal = object->add_item();
+      positionVal->set_type(NetOCE_Value::FLOAT_BUFFER);
+      positionVal->set_string_value("positions");
+      positionVal->set_bytes_value(position, position_size);
+      free(position);
+
+      NetOCE_Value *normalVal = object->add_item();
+      normalVal->set_string_value("normals");
+      normalVal->set_type(NetOCE_Value::FLOAT_BUFFER);
+      normalVal->set_bytes_value(normal, normal_size);
+      free(normal);
+
+      NetOCE_Value *domainVal = object->add_item();
+      domainVal->set_string_value("features");
+      domainVal->set_type(NetOCE_Value::UINT32_BUFFER);
+      domainVal->set_bytes_value(domains, domain_size);
+      free(domains);
+
+      NetOCE_Value *boundVal = object->add_item();
+      boundVal->set_string_value("bounds");
+      boundVal->set_type(NetOCE_Value::DOUBLE_BUFFER);
+      boundVal->set_bytes_value(bounds, sizeof(bounds));
     }
 
-    mesh->Clear();
     mesh.Nullify();
-    compoundShape.Nullify();
-
-    NetOCE_Value *object = res->add_value();
-    object->set_type(NetOCE_Value::OBJECT);
-
-
-    NetOCE_Value *positionVal = object->add_item();
-    positionVal->set_type(NetOCE_Value::FLOAT_BUFFER);
-    positionVal->set_string_value("positions");
-    positionVal->set_bytes_value(position, position_size);
-    free(position);
-
-    NetOCE_Value *normalVal = object->add_item();
-    normalVal->set_string_value("normals");
-    normalVal->set_type(NetOCE_Value::FLOAT_BUFFER);
-    normalVal->set_bytes_value(normal, normal_size);
-    free(normal);
-
-    NetOCE_Value *domainVal = object->add_item();
-    domainVal->set_string_value("features");
-    domainVal->set_type(NetOCE_Value::UINT32_BUFFER);
-    domainVal->set_bytes_value(domains, domain_size);
-    free(domains);
-
-    NetOCE_Value *boundVal = object->add_item();
-    boundVal->set_string_value("bounds");
-    boundVal->set_type(NetOCE_Value::DOUBLE_BUFFER);
-    boundVal->set_bytes_value(bounds, sizeof(bounds));
-
   } else {
     HANDLER_ERROR("please make some shapes first!")
   }
