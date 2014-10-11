046933227b31765d1c92ef752db69c6e11a132d5
Elijah Insua <tmpvar@gmail.com>
2014-10-10 20:03:35 -0700

    ship edges off to the client along (op_display)

--- protocol/handlers/shape_display.h
+++ protocol/handlers/shape_display.h
@@ -5,6 +5,102 @@
 #include <Bnd_Box.hxx>
 #include <StlMesh_Mesh.hxx>
 #include <StlMesh_MeshExplorer.hxx>
+#include <GCPnts_UniformAbscissa.hxx>
+#include <BRepAdaptor_Curve.hxx>
+#include <TopoDS.hxx>
+#include <GeomAdaptor_Curve.hxx>
+#include <BRep_Tool.hxx>
+
+#include <gp_Lin.hxx>
+#include <gp_Circ.hxx>
+#include <Geom_TrimmedCurve.hxx>
+#include <Geom_Line.hxx>
+
+void discretization (const TopoDS_Edge& edge, NetOCE_Value *oce_feature_edges) {
+
+  Standard_Real curve_start = 0, curve_end = 0;
+  Handle_Geom_Curve curve = BRep_Tool::Curve (edge, curve_start, curve_end);
+  assert (!curve.IsNull ());
+  GeomAdaptor_Curve geometry_curve_adaptator (curve);
+
+  GeomAbs_CurveType curve_type = geometry_curve_adaptator.GetType();
+
+  uint8_t point_size = sizeof(double) * 3;
+
+  NetOCE_Value *oce_edge = oce_feature_edges->add_item();
+  oce_edge->set_type(NetOCE_Value::OBJECT);
+  NetOCE_Value *oce_edge_type = oce_edge->add_item();
+  oce_edge_type->set_type(NetOCE_Value::PRIMITIVE);
+  oce_edge_type->set_string_value("type");
+
+  if (curve_type == GeomAbs_Line) {
+    gp_Lin line = geometry_curve_adaptator.Line();
+
+    Handle(Geom_Line) geom_line = new Geom_Line(line);
+    Geom_TrimmedCurve line_segment(geom_line, curve_start, curve_end, true);
+
+    gp_Pnt line_start = line_segment.StartPoint();
+    gp_Pnt line_end = line_segment.EndPoint();
+
+    oce_edge_type->set_prim_type(NetOCE_Value::LINE);
+
+
+    double line_start_d[3] = { line_start.X(), line_start.Y(), line_start.Z() };
+    NetOCE_Value *oce_line_start = oce_edge->add_item();
+    oce_line_start->set_type(NetOCE_Value::DOUBLE_BUFFER);
+    oce_line_start->set_string_value("start");
+    oce_line_start->set_bytes_value(line_start_d, point_size);
+
+    double line_end_d[3] = { line_end.X(), line_end.Y(), line_end.Z() };
+    NetOCE_Value *oce_line_end = oce_edge->add_item();
+    oce_line_end->set_type(NetOCE_Value::DOUBLE_BUFFER);
+    oce_line_end->set_string_value("end");
+    oce_line_end->set_bytes_value(line_end_d, point_size);
+
+  } else if (curve_type == GeomAbs_Circle) {
+    gp_Circ circle = geometry_curve_adaptator.Circle();
+    gp_Pnt loc = circle.Location();
+
+    oce_edge_type->set_prim_type(NetOCE_Value::CIRCLE);
+
+    double circle_center_d[3] = { loc.X(), loc.Y(), loc.Z() };
+    NetOCE_Value *oce_circle_center = oce_edge->add_item();
+    oce_circle_center->set_type(NetOCE_Value::DOUBLE_BUFFER);
+    oce_circle_center->set_string_value("center");
+    oce_circle_center->set_bytes_value(circle_center_d, point_size);
+
+    NetOCE_Value *oce_circle_radius = oce_edge->add_item();
+    oce_circle_radius->set_type(NetOCE_Value::DOUBLE);
+    oce_circle_radius->set_string_value("radius");
+    oce_circle_radius->set_double_value(circle.Radius());
+
+
+  } else {
+    fprintf(stderr, "UNHANDLED CURVE TYPE: %i\n", curve_type);
+
+    BRepAdaptor_Curve curve_adaptator (edge);
+    GCPnts_UniformAbscissa discretizer;
+    discretizer.Initialize (curve_adaptator, 0.5);
+
+    fprintf(stderr, "discretization (other):");
+
+    for (uint32_t i = 1; i <= discretizer.NbPoints (); i++) {
+      gp_Pnt p = curve_adaptator.Value (discretizer.Parameter (i));
+      fprintf(stderr, " (%f, %f, %f)", p.X(), p.Y(), p.Z());
+    }
+    fprintf(stderr, "\n");
+  }
+
+  /*
+    GeomAbs_Ellipse:
+    GeomAbs_Hyperbola:
+    GeomAbs_Parabola:
+    GeomAbs_BezierCurve:
+    GeomAbs_BSplineCurve:
+    GeomAbs_OtherCurve:
+  */
+}
+
 
 HANDLER(shape_display, "handle, handle..") {
 
@@ -60,7 +156,6 @@ HANDLER(shape_display, "handle, handle..") {
         bounds[5]-bounds[2]
       ) * 0.01;
 
-      // TODO: why am I newing here?
       StlTransfer::BuildIncrementalMesh(shape, .1, true, mesh);
 
       total_triangles = mesh->NbTriangles();
@@ -80,7 +175,29 @@ HANDLER(shape_display, "handle, handle..") {
       uint32_t *domains = (uint32_t *)malloc(domain_size);
       StlMesh_MeshExplorer explorer(mesh);
 
+      NetOCE_Value *object = res->add_value();
+      object->set_type(NetOCE_Value::OBJECT);
+
+
+      NetOCE_Value *oce_edges = object->add_item();
+      oce_edges->set_type(NetOCE_Value::ARRAY);
+      oce_edges->set_string_value("feature_edges");
+
+      TopExp_Explorer face_explorer(shape, TopAbs_FACE);
       for (uint32_t nbd = 1; nbd <= domainCount; nbd++) {
+
+        NetOCE_Value *oce_feature_edges = oce_edges->add_item();
+        oce_feature_edges->set_type(NetOCE_Value::ARRAY);
+
+        TopExp_Explorer edge_explorer(face_explorer.Current(), TopAbs_EDGE);
+        for (; edge_explorer.More(); edge_explorer.Next()) {
+          const TopoDS_Edge edge = TopoDS::Edge(edge_explorer.Current());
+
+          NetOCE_Value *oce_current_edge = oce_feature_edges->add_item();
+          oce_current_edge->set_type(NetOCE_Value::ARRAY);
+
+          discretization(edge, oce_current_edge);
+        }
 
         for (explorer.InitTriangle(nbd); explorer.MoreTriangle(); explorer.NextTriangle()) {
           explorer.TriangleVertices (
@@ -125,12 +242,13 @@ HANDLER(shape_display, "handle, handle..") {
           normal[normal_where++] = norm.Y();
           normal[normal_where++] = norm.Z();
         }
+
+        if (face_explorer.More()) {
+          face_explorer.Next();
+        }
       }
 
       mesh->Clear();
-
-      NetOCE_Value *object = res->add_value();
-      object->set_type(NetOCE_Value::OBJECT);
 
       NetOCE_Value *positionVal = object->add_item();
       positionVal->set_type(NetOCE_Value::FLOAT_BUFFER);
